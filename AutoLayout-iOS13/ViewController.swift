492ff4df5cb74abb6cc0e7ae4f5b6ba49519a9f6
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-21 09:57:00 +0100

    Update orientations

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = allDice[Int.random(in: 0...5)]
+        diceImageView2.image = allDice[Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
